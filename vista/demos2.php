20202591d5010a8fddfcb590353a3deb11e477bc
Robinzon Santana <sant4n4.1@gmail.com>
2019-09-30 15:35:48 -0500

    Editando el formulario de demo2

--- vista/demos2.php
+++ vista/demos2.php
@@ -14,7 +14,7 @@
 		<div class="col-md-12">
 			<blockquote class="blockquote">
 				<p class="mb-0">
-					I.- PARA MEDIANAS Y GRANDES EMPRESAS
+					III.- PARA MICROEMPRESA : (Menos de 150 IUTS)
 				</p>
 				<footer class="blockquote-footer">
 					SUBSISTEMA(MODULOS) DE :
@@ -25,84 +25,29 @@
 
     <div class="card mb-4">
         <div class="card-body">
-
-          <div class="row">
-                <div class="col-lg-3">
-                    <img class="img-fluid rounded" src="img/demoProduc.png" alt="">
-                    <span  class="badge badge-danger">( EDITORIAL - PAPELERIA Y OTROS )</span>
-                </div>
-                <div class="col-lg-9">
-                    <div class="row">
-                              <div class="col-md-6">   
-                                  <a href="../archivo/DemoProduc.exe" target="_blank" class="btn btn-block btn-outline-primary" title="Click para Descargar .exe" >
-                                  <img src="https://img.icons8.com/bubbles/100/000000/short-hair-girl-with-glasses-product-box.png"> Producción</a>
-                              </div>
-                              <div class="col-md-6">
-                                  <a href="../archivo/PCPE-PRODUCCION.pdf" target="_blank" class="btn btn-block btn-outline-danger" title="Click para Descargar .pdf" >
-                                  <img src="https://img.icons8.com/bubbles/100/000000/pdf-2.png"></a>                                                          
-                              </div>
-                    </div>
-                </div>
-          </div>
-          
-        </div>
-
-    </div>
-
-    <div class="card mb-4">
-        <div class="card-body">
            
 
-        <div class="row">
-              <div class="col-lg-3">
-                  <img class="img-fluid rounded" src="img/pventa.png" alt="">
-                  <span  class="badge badge-danger">( TIENDAS - FERIAS , FERRETERIAS )</span>
-              </div>
-              <div class="col-lg-9">
-                  <div class="row">
-                            <div class="col-md-6">   
-                                <a href="../archivo/DemoPVenta.exe" target="_blank" class="btn btn-block btn-outline-primary" title="Click para Descargar .exe" >
-                                <img src="https://img.icons8.com/plasticine/100/000000/billing-machine.png"> Punto de Venta</a>
-                            </div>
-                            <div class="col-md-6">
-                                <a href="../archivo/ISANEG-PUNTO_VENTA.pdf" target="_blank" class="btn btn-block btn-outline-danger" title="Click para Descargar .pdf" >
-                                <img src="https://img.icons8.com/bubbles/100/000000/pdf-2.png"></a>                                                          
-                            </div>
-                  </div>
-              </div>
-        </div>
-
-
-
-        </div>
-
-    </div>
-    <div class="card mb-4">
-        <div class="card-body">
-
-        <div class="row">
-                <div class="col-lg-3">
-                    <img class="img-fluid rounded" src="img/facturacion.png" alt="">
-                    <span  class="badge badge-danger">( SEDE CENTRAL, CONSIGNATARIOS )</span>
-                </div>
-                <div class="col-lg-9">
-                    <div class="row">
-                              <div class="col-md-6">   
-                                  <a href="../archivo/DemoFactu.exe" target="_blank" class="btn btn-block btn-outline-primary " title="Click para Descargar .exe" >
-                                  <img src="https://img.icons8.com/bubbles/100/000000/bill.png"> FACTURACIÓN</a>
-                              </div>
-                              <div class="col-md-6">
-                                  <a href="../archivo/ISANEG-FACTURACION.pdf" target="_blank" class="btn btn-block btn-outline-danger" title="Click para Descargar .pdf" >
-                                  <img src="https://img.icons8.com/bubbles/100/000000/pdf-2.png"></a>                                                          
-                              </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <img class="img-fluid rounded" src="img/pventa.png" alt="">
+                        <span  class="badge badge-danger">( TIENDAS - FERIAS , FERRETERIAS )</span>
+                    </div>
+                    <div class="col-lg-9">
+                        <div class="row">
+                                    <div class="col-md-6">   
+                                        <a href="../archivo/DemoPVenta.exe" target="_blank" class="btn btn-block btn-outline-primary" title="Click para Descargar .exe" >
+                                        <img src="https://img.icons8.com/plasticine/100/000000/billing-machine.png"> Punto de Venta</a>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <a href="../archivo/ISANEG-PUNTO_VENTA.pdf" target="_blank" class="btn btn-block btn-outline-danger" title="Click para Descargar .pdf" >
+                                        <img src="https://img.icons8.com/bubbles/100/000000/pdf-2.png"></a>                                                          
+                                    </div>
+                        </div>
                     </div>
                 </div>
         </div>
-
-          
-        </div>
-
     </div>
+
 
 </div>
 
